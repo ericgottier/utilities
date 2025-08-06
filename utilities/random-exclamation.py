e37f3140891da7e101f2fe70b47dca75bbeebfb1
ericgottier <ericgottier91@gmail.com>
2025-08-05 20:48:40 -0600

    Rewrote random-exclamation for improved efficiency in absurd character counts

--- utilities/random-exclamation.py
+++ utilities/random-exclamation.py
@@ -1,10 +1,12 @@
 import random
 
-chars = ['!', '?', '1']
+chars = ['!', '?', '1', '']
 
-excl = ''
-for i in range(random.randint(7, 50)):
-    choose = random.randint(0,2)
-    excl += chars[choose]
+collect_chars = []
+for i in range(random.randint(7, 500000)):
+    choose = random.randint(0,len(chars)-1)
+    collect_chars.append(chars[choose])
+    
+excl = ''.join(collect_chars)
 
 print(excl)
